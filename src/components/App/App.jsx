0f58861f3a80d2571615a1f5f08ef645075b95b5
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-09-29 23:43:25 +0300

    finish task Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+
 import friends from "../data/friends.json" 
+import user from "../data/user.json"
+
 import { FriendsList } from "../FriendsListSection/FriendsList/FriendsList"
+import {Profile} from "../Profile/Profile"
 
 export const App = () => {
   return (
@@ -9,11 +13,21 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        // alignItems: 'center',
-        // fontSize: 40,
+        fontSize: 40,
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        alignItems: 'center',
         color: '#010101'
       }}
     >
+      <Profile
+               username={user.username}
+               tag={user.tag}
+               location={user.location}
+               avatar={user.avatar}
+               stats={user.stats}
+      />
+
       <FriendsList friends={friends} />
     </div>
   );
