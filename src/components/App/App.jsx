43086470a7b62072c6855d2ed163a1abf7d9c2d5
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-09-30 00:38:23 +0300

    finish task Statistics section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-import friends from "../data/friends.json" 
 import user from "../data/user.json"
+import data from '../data/data.json';
+import friends from "../data/friends.json" 
 
+import { Profile } from "../Profile/Profile"
+import { Statistics } from "../SectionStatistics/Statistics"
 import { FriendsList } from "../FriendsListSection/FriendsList/FriendsList"
-import {Profile} from "../Profile/Profile"
+
+
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         fontSize: 40,
         flexDirection: 'column',
         flexWrap: 'nowrap',
         alignItems: 'center',
-        color: '#010101'
+        color: '#010101',
       }}
     >
       <Profile
@@ -27,6 +31,8 @@ export const App = () => {
                avatar={user.avatar}
                stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
 
       <FriendsList friends={friends} />
     </div>
