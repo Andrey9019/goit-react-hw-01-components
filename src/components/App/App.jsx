83e70c07ccd5be750d3db5d37f5e7c535cb79c8c
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-10-01 22:14:09 +0300

    update section statistics

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import user from "../data/user.json"
-import data from '../data/data.json';
-import friends from "../data/friends.json" 
-import transactions from '../data/transactions.json';
+import user from "../../data/user.json"
+import data from '../../data/data.json';
+import friends from "../../data/friends.json" 
+import transactions from '../../data/transactions.json';
 
 import { Profile } from "../Profile/Profile"
 import { Statistics } from "../SectionStatistics/Statistics"
--- src/components/SectionStatistics/Statistics.jsx
+++ src/components/SectionStatistics/Statistics.jsx
@@ -1,14 +1,22 @@
 import css from './Statistics.module.css'
 import {getRandomHexColor} from "./RandomColor/randomColor"
 
-export const Statistics = ({ title, stats, }) =>
-    
-{ return <section className={css.statistics}>
-    <h2 className={css.title}>{title}</h2>
-    <ul className={css.staList}>
-{stats.map(stat=> (<li className={css.item} key={stat.id} style={{backgroundColor:getRandomHexColor()}}>
-    <span className={css.label}>{stat.label} </span>
-    <span className={css.percentage}>{stat.percentage}%</span>
-        </li>)  )}
-    </ul>
-</section>}
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.staList}>
+        {stats.map((stat) => (
+          <li
+            className={css.item}
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}>
+            <span className={css.label}>{stat.label} </span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
